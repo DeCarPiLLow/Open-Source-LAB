d174c5259de70471aa86ec4e2401bda076804fa0
DeCarPiLLow <79994311+DeCarPiLLow@users.noreply.github.com>
2022-08-05 10:35:23 +0530

    Create q3.py

---
+++ q3.py
@@ -0,0 +1,12 @@
+def duplicate(list: int) -> boolean:
+  for i in range(10):
+    for j in range(10):
+      if list[i] == list[j] && i != j
+        return true
+  return false      
+      
+  
+ 
+  
+list = [1,2,3,4,5,6,7,8,9,1]
+print("Duplicate found: " duplicate(list))
