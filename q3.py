36c4087752be8284edc24d12666a39d62d57d6d9
DeCarPiLLow <79994311+DeCarPiLLow@users.noreply.github.com>
2022-08-05 10:42:30 +0530

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,12 +1,10 @@
-def duplicate(list: int) -> boolean:
+def duplicate(list: int) -> bool:
   for i in range(10):
     for j in range(10):
-      if list[i] == list[j] && i != j
-        return true
-  return false      
-      
-  
- 
-  
+      if i != j:
+        if list[i] == list[j]:
+          return True
+  return False      
+
 list = [1,2,3,4,5,6,7,8,9,1]
-print("Duplicate found: " duplicate(list))
+print(f"Duplicate found: {duplicate(list)}")
